fe77da5907541bb24c14a7e8db0b1310e491de9b
CarlosScaldaferri <carlosscaldaferri@gmaail.com>
2023-02-10 16:09:31 -0300

    Correção no zipcode

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,11 +3,9 @@ import { HeaderCenterDiv } from "./Style";
 import BrandLogo from "./brandLogo/BrandLogo";
 import UserLogin from "./userLogin/UserLogin";
 import GeneralItens from "./generalItens/GeneralItens";
-import ZipCode from "./ZipCode/ZipCode";
+import ZipCode from "./zipCode/ZipCode";
 import SearshInput from "./searshInput/SearshInput";
-import Nav from "./nav/Nav";
 import { ItensStyled } from "./Style";
-import { NavStyled } from "./Style";
 import { ItensContainerStyled } from "./Style";
 
 export default function Header(props) {
